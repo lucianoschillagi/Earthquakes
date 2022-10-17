ec8e9e70b11ce8a230376f7ce832ef3d35d3dbea
luko <luko.web@gmail.com>
2022-10-17 18:08:57 -0300

    [ADD] unit test to earthquake

--- EarthquakesTests/EarthquakesTests.swift
+++ EarthquakesTests/EarthquakesTests.swift
@@ -7,30 +7,25 @@
 //
 
 import XCTest
+@testable import Earthquakes
 
 final class EarthquakesTests: XCTestCase {
+    
+    func testGeoJSONDecoderDecodesQuake() throws {
+        let decoder = JSONDecoder()
+        // date decoding stratety -> millisecondsSince1970
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        // test quake code
+        XCTAssertEqual(quake.code, "73649170")
+        // test quake place
+        XCTAssertEqual(quake.place, "5km NW of The Geysers, CA")
+        // test quake time
+        let expectedSeconds = TimeInterval(1636129710550) / 1000
+        let decodedSeconds = quake.time.timeIntervalSince1970
+        XCTAssertEqual(expectedSeconds, decodedSeconds, accuracy: 0.00001)
+        
     }
 
 }
