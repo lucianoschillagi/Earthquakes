125b8900162536419cb24c1ec0277ab60fec4ddd
luko <luko.web@gmail.com>
2022-10-17 16:39:46 -0300

    [ADD] decoding structured json code

---
+++ QuakeError.swift
@@ -0,0 +1,25 @@
+//
+//  QuakeError.swift
+//  Earthquakes-iOS
+//
+//  Created by Luko on 17/10/2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString(
+                "Found and will discard a quake missing a valid code, magnitude, place, or time.",
+                comment: ""
+            )
+        }
+    }
+}
