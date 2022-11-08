1a9899a3183d2aaf999942283cace33f4eb41764
luko <luko.web@gmail.com>
2022-11-08 11:17:51 -0300

    [ADD] map view code

---
+++ QuakeDetailMap.swift
@@ -0,0 +1,46 @@
+//
+//  QuakeDetailMap.swift
+//  Earthquakes-iOS
+//
+//  Created by Luko on 08/11/2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct QuakeDetailMap: View {
+    let location: QuakeLocation
+    let tintColor: Color
+    private var place: QuakePlace
+    
+    @State private var region = MKCoordinateRegion()
+    
+    init(location: QuakeLocation, tintColor: Color) {
+        self.location = location
+        self.tintColor = tintColor
+        self.place = QuakePlace(location: location)
+    }
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .onAppear {
+                withAnimation {
+                    region.center = place.location
+                    region.span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+            }
+        }
+    }
+}
+
+struct QuakePlace: Identifiable {
+    let id: UUID
+    let location: CLLocationCoordinate2D
+    
+    init(id: UUID = UUID(), location: QuakeLocation) {
+        self.id = id
+        self.location = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+    }
+}
+
